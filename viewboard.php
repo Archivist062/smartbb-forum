3ee57c9abee8e101546dcaf0af2a762b226b42c8
Alex Abbott <smartboyathome@gmail.com>
2009-10-13 17:02:19 -0700

    Spaces replace tabs

--- viewboard.php
+++ viewboard.php
@@ -1,16 +1,16 @@
 <?php $pageTitle = "Test Forum: " . $_GET['board']; include("../theme/header.php");
 
-	$groupname = $_GET['group'];
-	$boardname = $_GET['board'];
-	$directory = getcwd() . "/boards/$groupname/$boardname";
-	$directory_handle = @opendir($directory) or die("Error opening $directory");
-	echo "<h3><a href=\"/forum/index.php\">Test Forum</a> > $boardname</h3><br><table class=\"board\"><tbody><tr><td>Topic Title</td><td>Replies</td></tr>";
-	while ($topic = readdir($directory_handle)) {
-		if($topic == "." || $topic == ".." || $topic == "settings") { continue; }
-		$postCount = count(glob("$directory/$topic/reply*"))+1;
-		echo "<tr><td><a href=\"viewtopic.php?group=$groupname&board=$boardname&topic=$topic\">$topic</a></td><td>$postCount</td></tr>";
-	}
-	echo "</tbody></table>";
-	closedir($directory_handle);
+    $groupname = $_GET['group'];
+    $boardname = $_GET['board'];
+    $directory = getcwd() . "/boards/$groupname/$boardname";
+    $directory_handle = @opendir($directory) or die("Error opening $directory");
+    echo "<h3><a href=\"/forum/index.php\">Test Forum</a> > $boardname</h3><br><table class=\"board\"><tbody><tr><td>Topic Title</td><td>Replies</td></tr>";
+    while ($topic = readdir($directory_handle)) {
+        if($topic == "." || $topic == ".." || $topic == "settings") { continue; }
+        $postCount = count(glob("$directory/$topic/reply*"))+1;
+        echo "<tr><td><a href=\"viewtopic.php?group=$groupname&board=$boardname&topic=$topic\">$topic</a></td><td>$postCount</td></tr>";
+    }
+    echo "</tbody></table>";
+    closedir($directory_handle);
 
 include("../theme/footer.php"); ?>
--- viewtopic.php
+++ viewtopic.php
@@ -1,24 +1,24 @@
 <?php $pageTitle = "Test Forum: " . $_GET['topic']; include("../theme/header.php");
 
-	$groupname = $_GET['group'];
-	$boardname = $_GET['board'];
-	$topicname = $_GET['topic'];
-	$directory = getcwd() . "/boards/$groupname/$boardname/$topicname";
-	$postCount = count(glob("$directory/$topic/reply*"))+1;
-	$directory_handle = @opendir($directory) or die("Error opening $directory");
-	echo "<h3><a href=\"/forum/index.php\">Test Forum</a> > <a href=\"/forum/viewboard.php?group=$groupname&board=$boardname\">$boardname</a> > $topicname</h3>";
-	$a = 0;
-	echo "<table class=\"post\" style=\"width:750px; max-width:750px;\"><tbody>";
-	while ($a != $postCount) {
-		if($a == 0) {
-			$post = file("$directory/topic");
-		} else {
-			$post = file("$directory/reply$a");
-		}
-		echo "<tr><td class=\"postAuthor\">".$post['1']."</td><td class=\"postDate\" align=\"right\" nowrap>".date('h:i:s M d Y',$post['2'])."</td></tr><tr><td class=\"postMessage\" colspan=\"2\">".$post['0']."</td></tr>";
-		$a++;
-	}
-	echo "</tbody></table>";
-	closedir($directory_handle);
+    $groupname = $_GET['group'];
+    $boardname = $_GET['board'];
+    $topicname = $_GET['topic'];
+    $directory = getcwd() . "/boards/$groupname/$boardname/$topicname";
+    $postCount = count(glob("$directory/$topic/reply*"))+1;
+    $directory_handle = @opendir($directory) or die("Error opening $directory");
+    echo "<h3><a href=\"/forum/index.php\">Test Forum</a> > <a href=\"/forum/viewboard.php?group=$groupname&board=$boardname\">$boardname</a> > $topicname</h3>";
+    $a = 0;
+    echo "<table class=\"post\" style=\"width:750px; max-width:750px;\"><tbody>";
+    while ($a != $postCount) {
+        if($a == 0) {
+            $post = file("$directory/topic");
+        } else {
+            $post = file("$directory/reply$a");
+        }
+        echo "<tr><td class=\"postAuthor\">".$post['1']."</td><td class=\"postDate\" align=\"right\" nowrap>".date('h:i:s M d Y',$post['2'])."</td></tr><tr><td class=\"postMessage\" colspan=\"2\">".$post['0']."</td></tr>";
+        $a++;
+    }
+    echo "</tbody></table>";
+    closedir($directory_handle);
 
 include("../theme/footer.php"); ?>
